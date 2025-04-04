63d02f75ebef11ddbd90ce052e86ab991dfacacf
Kirk Lin <linkirk@163.com>
2025-04-05 00:52:28 +0800

    fix: resolve TypeScript errors in i18n configuration and storage utility

--- src/lib/i18n/index.tsx
+++ src/lib/i18n/index.tsx
@@ -12,7 +12,7 @@ i18n.use(initReactI18next).init({
   resources,
   lng: getLanguage() || locale, // TODO: if you are not supporting multiple languages or languages with multiple directions you can set the default value to `en`
   fallbackLng: "en",
-  compatibilityJSON: "v3", // By default React Native projects does not support Intl
+  compatibilityJSON: "v4", // By default React Native projects does not support Intl
 
   // allows integrating dynamic values into translations.
   interpolation: {
